3f29d59d7e401b90b20b97c9d90d5c6c63864382
Flying Dutchman <brian.vanhoff@gmail.com>
2020-06-26 16:21:01 -0400

    dry'ed the code and edited comments

--- cron/get_and_make.py
+++ cron/get_and_make.py
@@ -1,3 +1,8 @@
+''' A single function to request weather data from the OpenWeatherMaps API
+service, transform the returned data into larger, structured documents, load
+the requested data and incomplete documents to the local MongoDB database, and
+finally load any complete documents to a separate database. '''
+
 import os
 import time
 import json
@@ -5,7 +10,6 @@ import json
 from pymongo import MongoClient
 
 import request_and_load
-from request_and_load import read_list_from_file
 import weather
 import db_ops
 import make_instants
@@ -15,6 +19,16 @@ from config import OWM_API_key_masta as masta_key
 from config import port, host #, user, password, socket_path
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def read_list_from_file(filename):
+    """ Read the zip codes list from the csv file.
+        
+    :param filename: the name of the file
+    :type filename: sting
+    """
+    with open(filename, "r") as z_list:
+        return z_list.read().strip().split(',')
 
 def get_and_make(codes):
     ''' Request weather data from the OWM api. Transform and load that data
@@ -26,46 +40,39 @@ def get_and_make(codes):
 
     # Begin a timer for the process and run the request and load process.
     start_start = time.time()
-    print(f'task began at {start_start}')
+    print(f'Weather ETL process began at {start_start}')
     i, n = 0, 0 # i for counting zipcodes processed and n for counting API
                 # calls made; API calls limited to a maximum 60/minute/apikey.
     start_time = time.time()
     for code in codes:
         try:
             current = weather.get_current_weather(code)
-#             print(current)
-#             exit()
-            ### If the data was not recieved a -1 should have been returned.
-            ### When this happens it should be handled some way other than
-            ### just skipping over it. And the try block is not necessary here.
+            n+=1 
+            forecasts = weather.five_day(current.loc) # location=current_coords 
+            n+=1
         except AttributeError as e:
-            print(e)
             print(f'AttributeError for {code}. Continuing to next code.')
-#             exit()
             continue
-        n+=1
-        coords = current.loc ###['coordinates']
-        try:
-            forecasts = weather.five_day(coords)
-        except AttributeError:
-            print(f'got AttributeError for {coords}. Continuing to next code.')
-            continue
-        n+=1
-        
-        ### This should load to database the old way if the data comes in as
-        ### the old way (that is, as strait up dict or list) but switch to the
-        ### new way if the data was collected and returned as Weather objects.
+
+        # Try to load the data in the weather.Weather objects. If it can't, do
+        # load it the old way in case current and forecasts are dict and list.
         try:
             db_ops.load(current.as_dict, client, config.database, 'obs_temp')
             for cast in forecasts:
                 db_ops.load(cast.as_dict, client, config.database, 'cast_temp')
         except:
-            request_and_load.load_weather(current, client, config.database, 'obs_temp')
-            request_and_load.load_weather(forecasts, client, config.database, 'cast_temp')
+            print(f'''There was an error while get_and_make.get_and_make() was
+            attempting to load to {client}. Now trying to use request_and_load.
+            load_weather() to do the same thing.''')
+            request_and_load.load_weather(current, client, \
+                                          config.database, 'obs_temp')
+            request_and_load.load_weather(forecasts, client, \
+                                          config.database, 'cast_temp')
 
-        # if the api request rate is greater than 60 just keep going. Otherwise
-        # check how many requests have been made and if it's more than 120
-        # start make_instants.
+        # If the api request rate is greater than 60 just keep requesting.
+        # Otherwise check how many requests have been made, and if it's more
+        # than 120 start sorting the data to instants while you reduce the
+        # request rate by waiting.
         if n/2 / (time.time()-start_time) <= 1:
             i+=1
             continue
@@ -79,24 +86,20 @@ def get_and_make(codes):
                     start_time = time.time()
                 n = 0
 
-    # sort the last of the documents in temp collections
+    # Sort the last of the documents in temp collections
     try:
         make_instants.make_instants(client)
     except:
         print('No more documents to sort into instants')
-    print(f'task took {time.time() - start_start}sec and processed {i} codes')
+    print(f'''Weather ETL process has concluded.
+    It took {time.time() - start_start} seconds and processed {i} locations''')
 
 if __name__ == '__main__':
     # This try block is to deal with the switching back and forth between
     # computers with different directory names.
-    try:
-        directory = os.path.join(os.environ['HOME'], 'data', 'forecast-forecast')
-        filename = os.path.join(directory, 'ETL', 'Extract', 'resources', 'success_zipsNC.csv')
-        codes = read_list_from_file(filename)
-    except FileNotFoundError:
-        directory = BASE_DIR
-        filename = os.path.join(directory, 'ETL', 'Extract', 'resources', 'success_zipsNC.csv')
-        codes = read_list_from_file(filename)
+    directory = os.path.join(os.environ['HOME'], 'data', 'forecast-forecast')
+    filename = os.path.join(directory, 'ETL', 'Extract', 'resources', 'success_zipsNC.csv')
+    codes = read_list_from_file(filename)
     client = MongoClient(host=host, port=port)
     get_and_make(codes)
 #     get_and_make(codes[:61])
