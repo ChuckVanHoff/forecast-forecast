df21d692ac7536b0caf473aed5b408f511f15f55
Brian VanHoff <brian.vanhoff@gmail.com>
2020-06-26 20:46:05 -0400

    restructuring and shifting and cleaning

--- ETL/instant.py
+++ ETL/instant.py
@@ -1,7 +1,14 @@
-''' Defines the Instant class and some useful functions. '''
+''' Defines the Instant class and some useful functions. When executed as main
+this module will search the database for completed and incompletable Instants
+and move the completed to a remote database-collection, legit_inst, and delete
+all the incompletable instants.'''
 
 
 class Instant:
+    ''' The Instant is an object built out of a collection of Weathers: in the
+    case of forecast-forecast there are 40 forecast dictionaries contained in a
+    list and one observation dictionary for comparison.
+    '''
 
     def __init__(self, _id, forecasts=[], observations={}):
         
@@ -21,11 +28,12 @@ class Instant:
     
     @property
     def itslegit(self):
-        ''' Check the instant's weathers array's count and if it is 40, then the
-        document is returned.
+        ''' Check the instant's weathers array's count and if it is 40, then
+        True is returned, otherwise it returns False.
 
         :param instant: the instant docuemnt to be legitimized
         :type instant: dictionary
+        :return: Boolian value (True or False)
         '''
         
         self.count()
@@ -46,22 +54,12 @@ class Instant:
 
         col = dbncol(client, collection, database=database)
         col.update_one({'_id': self._id}, {'$set': self.as_dict}, upsert=True)
-        
-    def as_delta(self):
-        ''' Create an Instant delta object. It finds the delta between the 
-        observation and each forecast and returns a list of deltas. '''
-        
-        from delta import make_delta
-        
-        # Loop through the forecasts array and append the return of make_delta()
-        # to and deltas list before returning that list.
-        return [make_delta(cast, self.obs) for cast in self.casts]
 
     
 def cast_count_all(instants):
     ''' get a tally for the forecast counts per document 
 
-    :param instants: docmuments loaded from the db.instants collection ### NOT
+    :param instants: docmuments loaded from the db.instants collection
     Instant class objects
     :type instants: list
     '''
@@ -86,20 +84,22 @@ def cast_count_all(instants):
 def sweep(instants):
     ''' Move any instant that has a ref_time less than the current next
     ref_time and with self.count less than 40. This is getting rid of the
-    instnats that are not and will never be legit. 
+    instants that are not and will not ever be legit.
 
-    :param instants: a list of Instant objects
+    :param instants: a itterable of Instant objects
+    :type instants: dict, list, pymongo cursor
     '''
     
     import time
     
     import pymongo
+    from pymongo import MongoClient
     from pymongo.cursor import Cursor
-    from Extract.make_instants import find_data
-    from config import client, database
-    from db_ops import dbncol
+    import config
+    import db_ops
     
-    col = dbncol(client, 'instant_temp', database=database)
+    client = MongoClient('localhost', 27017)
+    col = db_ops.dbncol(client, 'instant_temp', config.database)
     n = 0
     # Check the instant type- it could be a dict if it came from the database,
     # or it could be a list if it's a bunch of instant objects, or a pymongo
@@ -139,97 +139,43 @@ def find_legit(instants):
     ### methods you've been writing.
 
 
-def load_legit(legit_list):
+def load_legit(legit):
     ''' Load the 'legit' instants to the remote database and delete from temp.
-    This process does not delete the 
+    This process does not delete the documents upon insertion, but rather holds
+    it until the next time, tries to load it then, and finally, when getting a duplicate
+    key error, deletes the document from its temporary location.
 
-    :param collection: the collection you want to pull instants from
-    :type collection: pymongo.collection.Collection
+    :param legit: a single document
+    :type legit: dict
     '''
 
+    from pymongo import MongoClient
     from pymongo.errors import DuplicateKeyError
-    from config import client, database
-    from db_ops import dbncol
-    from db_ops import copy_docs
+
+    import config
+    import db_ops
     
-### this should load to legit_inst in owmap for production ###
-#         col = dbncol(client, 'legit_inst', 'owmap')
-    col = dbncol(client, 'legit_inst', database=database)
+    client = MongoClient('localhost', 27017)
+    remote_col = db_ops.dbncol(db_ops.Client(config.uri),'legit_inst', config.database)
+    col = db_ops.dbncol(client, 'instant_temp', config.database)
+    # col = dbncol(client, 'legit_inst', config.database)
     try:
-        col.insert_one(legit_list)
+        check = remote_col.insert_one(legit)
+        if not check:
+            print('was not able to insert to remote db')
     except DuplicateKeyError:
-        col = dbncol(client, 'instant_temp', database=database)
-        col.delete_one(legit_list)
-        ### saved for later, when doing it on bulk ###
+        col.delete_one(legit)
+#     ### saved for later, when doing it on bulk ###
 #     col.insert_many(legit_list)
-    # Now go to the temp_instants collection and delete the instants just
-    # loaded to legit_inst.
+#     # Now go to the temp_instants collection and delete the instants just
+#     # loaded to legit_inst.
 #     col.delete_many(legit_list)
+#     ### saved for later, when doing it on bulk ###
     return
-
-def make_delta(cast, obs):
-    ''' Compare the values of two dicts, key by key. When the values are numbers
-    return the difference, when strings return 0 if the strings are equal and 1
-    if they are different, when dicts run this function, when NoneType set the
-    value to 99999.
-    
-    :params cast, obs: dictionaries with the same set of keys and sub-keys
-    :type cas, obs: dict
-    '''
-    
-    delta = {}  # The delta document. Contains all the forecast errors
-    
-    for (k, v) in cast.items():
-        try:
-            # Check and compare dictionaries according to their value type
-            if type(v) == int or type(v) == float:
-                if type(obs[k]) == int or type(obs[k]) == float:
-                    delta[k] = v - obs[k]
-                    continue
-            elif type(v) == dict:
-                delta[k] = make_delta(v, obs[k])
-                continue
-            elif type(v) == str:
-                if v == obs[k]:
-                    delta[k] = 0
-                else:
-                    delta[k] = 1
-            elif type(v):
-                delta[k] = 999999
-                continue
-            else:
-                print(f'there was some other condition not met by the other\
-                checks. Look at {k} and {v}')
-                continue
-        except KeyError as e:
-            print(f'Caught a KeyError..... {e}')
-            # Add whichever key and value needs adding to the delta
-            if k not in obs and '1h' in obs:
-                delta['1h'] = obs['1h']
-                delta[k] = v
-            elif k not in obs and '3h' in obs:
-                delta['3h'] = obs['3h']  
-                delta['1h'] = v
-            continue
-    return delta
-
-def doc_to_inst(doc):
-    ''' Take a document from the instants database and make an Instant object
-    out of it.
-    
-    :param doc: a document from the owmap.legit_inst database
-    :type doc: dictionary
-    '''
-    
-    _id = f"{doc['instant']}{doc['zipcode']}"
-    forecasts = doc['forecasts']
-    observations = doc['weather']
-    return Instant(_id, forecasts, observations)
 
 
 import time
-start_time = time.time() # This is to get the total runtime if this script is
-                         # run as __main__
+
 if __name__ == '__main__':
     ''' Connect to the database, then move all the legit instants to the remote
     database and clear out any instants that are past and not legit.
@@ -238,12 +184,13 @@ if __name__ == '__main__':
     import config
     import db_ops
 
-    # Set the database here if you must, but it's better to do that in the
-    # config.py file
-#     database = 'owmap'
+    start_time = time.time() # This is to get the total runtime if this script is
+                             # run as __main__
+    print('Database sweep in progress...')
     collection = 'instant_temp'
-    col = db_ops.dbncol(config.client, collection, database=config.database)
+    col = db_ops.dbncol(db_ops.Client(config.uri),
+                        collection,
+                        config.database)
     cast_count_all(col.find({}))
     sweep(col.find({}))
-
-    print(f'Total op time for instant.py was {time.time()-start_time} seconds')
+    print(f'Total sweep time was {time.time()-start_time} seconds')
---
+++ ETL/make_instants.py
@@ -0,0 +1,152 @@
+''' Make the instant documents. Pull all documents from the "forecasted" and
+the "observed" database collections. Sort those documents according to the
+type: forecasted documents get their forecast arrays sorted into forecast lists
+within the documents having the same zipcode and instant values, observed
+documents are inserted to the same document corrosponding to the zipcode and
+instant values. '''
+
+
+import time
+
+import pymongo
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+import db_ops
+import config
+
+# use the local host and port for all the primary operations
+port = config.port #27017
+host = config.host #'localhost'
+client = MongoClient(host=host, port=port)
+
+def find_data(client, database, collection, filters={}):
+    ''' Find the items in the specified database and collection using the filters.
+
+    :param client: a MongoClient instance
+    :type client: pymongo.MongoClient
+    :param database: the name of the database to be used. It must be a database
+    name present at the client
+    :type database: str
+    :param collection: the database collection to be used.  It must be a
+    collection name present in the database
+    :type collection: str
+    :param filters: the parameters used for filtering the returned data. An
+    empty filter parameter returns the full collection
+    :type filters: dict
+    
+    :return: the result of the query
+    :type: pymongo.cursor.CursorType
+    '''
+
+    db = Database(client, database)
+    col = Collection(db, collection)
+    return col.find(filters).batch_size(100)
+
+def update_command_for(data):
+    ''' the 'update command' is the MongoDB command that is used to update data
+    should be a weather type object. it will have its filter and update set
+    according to the entry content. It returns a command to update in a pymongo
+    database.
+
+    :param data: the data dictionary created from the api calls
+    :type data: dict
+    :return: the command that will be used to find and update documents
+    ''' 
+    
+    
+    if data['_type'] == 'forecast':
+        filters = {'_id': data['_id']}
+        updates = {'$push': {'forecasts': data}} # append to forecasts list
+        return pymongo.UpdateOne(filters, updates,  upsert=True)
+    elif data['_type'] == 'observation':
+        filters = {'_id': data['_id']}
+        updates = {'$set': {'observations': data}}
+        return pymongo.UpdateOne(filters, updates,  upsert=True)
+    else:
+        print('There was neither "forecast" nor "observations" as "_type".')
+
+def make_load_list_from_cursor(cursor):
+    ''' create the list of objects from the database to be loaded through
+    bulk_write()
+    
+    :param cursor: it is just what the name says it is
+    :type cursor: a pymongo cursor
+    :return update_list: list of update commands for the weather objects on the
+    cursor
+    '''
+
+    update_list = []
+    
+    try:
+        n=0
+        for obj in cursor:
+            update_list.append(update_command_for(obj))
+        return update_list
+    except:
+        print('Error making load_list')
+        exit()
+        
+def copy_docs(col, destination_db, destination_col, filters={}, delete=False):
+    ''' Move or copy a collection within and between databases. 
+    
+    :param col: the collection to be copied
+    :type col: a pymongo collection or 
+    :param destination_col: the collection you want the documents copied into
+    :type destination_col: a pymongo.collection.Collection object
+    :param destination_db: the database with the collection you want the
+    documents copied into
+    :type destination_db: a pymongo database pymongo.databse.Database
+    :param filters: a filter for the documents to be copied from the collection.
+    :type filters: dict
+    :param delete: Determine if the orignials should be deleted. Default is no.
+    :type delete: bool
+    '''
+
+    copy = []
+    for item in col.find(filters):
+        copy.append(item)
+    destination = db_ops.dbncol(client, destination_col, destination_db)    
+    try:
+        inserted_ids = destination.insert_many(copy).inserted_ids
+        if delete == True:
+            # remove all the documents from the original collection
+            for row in inserted_ids:
+                filters = {'_id': row}
+                col.delete_one(filters)
+    except pymongo.errors.BulkWriteError as e:
+        print(f'The documents have not been copied to {destination_col}.')
+        print(e)
+    return
+
+def make_instants(client):
+    ''' Make the instant documents, as many as you can, with the data in the
+    named database. 
+    
+    - Process: Connect to the database, get the data from the database, make a
+    list lf load commands to get each document sorted into its proper instant
+    document, and finally move the sorted documents to their archives.
+    
+    :param client: a MongoDB client
+    :type client: pymongo.MongoClient
+    '''
+    
+    #Get the data
+    cast_col = db_ops.dbncol(client, "cast_temp", config.database)
+    obs_col = db_ops.dbncol(client, "obs_temp", config.database)
+    inst_col = db_ops.dbncol(client, "instant_temp", config.database)
+    forecasts = cast_col.find({})
+    observations = obs_col.find({})
+    
+    inst_col.create_index([('timeplace', pymongo.DESCENDING)])
+    
+    # make the load lists and load the data
+    cast_load_list = make_load_list_from_cursor(forecasts)
+    obs_load_list = make_load_list_from_cursor(observations)
+    inst_col.bulk_write(cast_load_list)
+    inst_col.bulk_write(obs_load_list)
+    
+    # Copy the docs to archive storage and delete the source data.
+    copy_docs(cast_col, config.database, 'cast_archive', delete=True)
+    copy_docs(obs_col, config.database, 'obs_archive', delete=True)
+    return
