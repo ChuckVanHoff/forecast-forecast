d89964053dd73819aa8ceb3b583179989e620353
Brian VanHoff <brian.vanhoff@gmail.com>
2020-11-02 21:02:50 -0500

    added after_party.py- it transfers all the data from remote to local

---
+++ ETL/owm_direct/after_party.py
@@ -0,0 +1,38 @@
+''' Move the data from the remote database to the local database and 
+erase the moved documents from the database. Also update the timeplace
+collection log.
+'''
+
+
+import json
+from collections import defaultdict
+
+import config
+import db_ops
+
+
+remote_col = config.remote_client[config.database][config.weathers_collection]
+local_col = config.client[config.database][config.weathers_collection]
+# Load the timeplace record and store it in a defaultdict to be updated.
+with open('timeplace_records.json', 'r') as fp:
+    tprec = json.load(fp)
+timeplace_record = defaultdict(int, tprec)
+
+
+if __name__ == '__main__':
+    # Update the timeplace collection record.
+    docs = remote_col.find({})
+    for doc in docs:
+        timeplace = str(doc['timeplace'])
+        timeplace_record[timeplace] += 1
+
+    db_ops.copy_docs(
+        remote_col,
+        config.client,
+        config.database,
+        config.weathers_collection,
+        filters={},
+        delete=True
+    )
+    with open('timeplace_records.json', 'w') as rec:
+        json.dump(timeplace_record, rec)
--- ETL/owm_direct/db_ops.py
+++ ETL/owm_direct/db_ops.py
@@ -1,6 +1,7 @@
 ''' Useful functions for forecast-forecast specific operations '''
 
 import time
+import collections
 
 from pymongo import MongoClient
 from pymongo.database import Database
@@ -126,7 +127,7 @@ def read_mongo_to_dict(collection, query={}, limit=None):
         cursor = collection.find(query).batch_size(100)
     return {curs.pop('_id'): curs for curs in cursor}
 
-def load(data, database, collection):
+def load(data, database, collection, client=config.client):
     ''' Load data to specified database collection. Also checks for a
     preexisting document with the same instant and zipcode, and updates it in
     the case that there was already one there.
@@ -141,7 +142,6 @@ def load(data, database, collection):
     :type collection: str
     '''
 
-    client = MongoClient(host, port)
     col = dbncol(client, collection, database)
 
     # set the appropriate database collections, filters and update types
@@ -221,65 +221,74 @@ def load(data, database, collection):
 #         print(f'The documents have not been copied to {destination_col}.')
 #         print(e)
 #     return
-def copy_docs(col, destination_db, destination_col, filters={}, delete=False):
+def copy_docs(col, destination_client, destination_db, destination_col, filters={}, delete=False):
     ''' Move or copy a collection within and between databases.
     
-    :param col: the collection to be copied
-    :type col: a pymongo collection
-    :param destination_col: the collection you want the documents copied into
-    :type destination_col: a pymongo.collection.Collection object
-    :param destination_db: the database you want the documents copied into
-    :type destination_db: a pymongo database pymongo.database.Database
+    :param col: The collection to be copied from.
+    :type col: pymongo.collection.Collection
+    :param destination_col: The name of the collection you want the documents
+        copied into.
+    :type destination_col: string
+    :param destination_db: The name of the database you want the documents
+        copied into.
+    :type destination_db: string
     :param filters: a filter for the documents to be copied from the collection
     By default all collection docs will be copied
     :type filters: dict
     '''
     
     n = 0
-    count = col.count_documents(filters)
-    original = col.find(filters).batch_size(100)
-    copy = []
+    ids = []  # For holding filters for the delete function.
+    count = col.count_documents(filters)  # For keeping list slices in range.
+    # Define the database and collections.
+    client = destination_client
+    database = destination_db
+    collection = destination_col
+    destination = dbncol(client, collection, database)
+
     ### Attempting to wrap this in a while-loop to break it up and save my RAM
     ### and swap memories.
-    while original.alive:
+    while n <= count:
+        # Since updating the source with each pass through the loop, there is 
+        # also a need to perform a new query each pass.
+        original = col.find(filters)
+        temp = []
+        ids = []
+        
+        # Take the next 100 documents or take all the way to the end.
         if n+100 <= count:
-            ### DO YOU REALLY NEED TO PUT THESE INTO A COPY, OR CAN YOU INSERT
-            ### THEM RIGHT OFF THE CURSOR?
-            for item in original[n:n+100]:
-                copy.append(item)
+            for item in original[:n+100]:
+                temp.append(item)
             n += 100
-            # Now do everything you need to do to copy the set of documents.
-            database = destination_db     # Define database and collection
-            collection = destination_col  # for the following operations.
-            destination = dbncol(config.client, collection, database)
-            inserted_ids = destination.insert_many(copy).inserted_ids
-            if delete == True:
-                # remove all the inserted documents from the origin collection.
-                for item in inserted_ids:
-                    filt = {'_id': item}
-                    col.delete_one(filt)
         else:
             for item in original[n:]:
-                copy.append(item)
-            # Now do everything you need to do to copy the set of documents.
-            database = destination_db     # Define database and collection
-            collection = destination_col  # for the following operations.
-            destination = dbncol(config.client, collection, database)
+                temp.append(item)
+            n = count + 1  # Incriment this to stop the loop.
+
+        # Now do everything you need to do to copy the set of documents and
+        # delete the originals when commanded.
+        if not temp:
+            break
+        try:
+            inserted_ids = destination.insert_many(
+                temp, 
+                bypass_document_validation=True,
+                ordered=False
+            ).inserted_ids
+        except BulkWriteError as bwe:
+            inserted_ids = []
+        if delete == True and inserted_ids:
+
+            # Enter ids into a dictionary for the delete function and delete.
+            for item in inserted_ids:
+                filt = {'_id': item}
+                ids.append(filt)
             try:
-                inserted_ids = destination.insert_many(copy, ordered=False).inserted_ids
-                if delete == True:
-                    # remove all the inserted documents from the origin collection.
-                    for item in inserted_ids:
-                        filt = {'_id': item}
-                        col.delete_one(filt)
+                for f in ids:
+                    result = col.delete_many(f)
             except BulkWriteError as bwe:
                 with open('bwe_log.txt', 'a') as log:
                     log.write(time.ctime())
                     log.write(str(bwe.details))
-#     if delete == True:
-#         print(f'MOVED docs from {col} to {destination}.')
-#     else:
-#         print(f'COPIED docs in {col} to {destination}.')
+        original = col.find(filters)  # Do a new 
     return
-### I know that I am not using this function, but I made a few edits in the 
-### case that I do need to use it at some time. See above in the un-commented.
