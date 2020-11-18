791253807f8dd11c0a04817047c578ec676f23c6
Brian VanHoff <brian.vanhoff@gmail.com>
2020-11-18 07:05:56 -0500

    deleted imake_df

--- ETL/owm_direct/after_party.py
+++ ETL/owm_direct/after_party.py
@@ -12,44 +12,47 @@ import config
 import db_ops
 
 
+timeplace_record = {}
 command = 'mongodump --uri=mongodb+srv://chuckvanhoff:Fe7ePrX%215L5Wh6W@cluster0-anhr9.mongodb.net/test'
 remote_col = config.remote_client[config.database][config.weathers_collection]
 local_col = config.client[config.database][config.weathers_collection]
-# This is in case the process did not finish clearing the remote database..
-try:
-    if remote_col.count_documents({}) != 0:
-        os.system(command):
-        print('performed the command.')
-    else:
-        print(f'{remote_col} is empty. Proceding with the weather collection.')
-except:
-    print('there was an exception')
-    pass
-# Load the timeplace record and store it in a defaultdict to be updated.
-try:
-    with open('timeplace_records.json', 'r') as fp:
-        tprec = json.load(fp)
-except:
-    with open('timeplaces_records.json', 'a') as fp:
-        print('Had to create timeplaces_records.json.')
-        tprec = {}
-timeplace_record = defaultdict(int, tprec)
 
 
 if __name__ == '__main__':
-    # Update the timeplace collection record.
+    # Load the timeplace record and store it in a defaultdict to be updated.
+    try:
+        with open('timeplace_records.json', 'r') as fp:
+            tprec = json.load(fp)
+        timeplace_record = defaultdict(int, tprec)
+        # Update the timeplace collection record.
+    except:
+        with open('timeplaces_records.json', 'a') as fp:
+            print('Had to create timeplaces_records.json.')
+            tprec = {}
     docs = remote_col.find({})
     for doc in docs:
         timeplace = str(doc['timeplace'])
         timeplace_record[timeplace] += 1
-
-    db_ops.copy_docs(
-        remote_col,
-        config.client,
-        config.database,
-        config.weathers_collection,
-        filters={},
-        delete=True
-    )
     with open('timeplace_records.json', 'w') as rec:
         json.dump(timeplace_record, rec)
+
+    # This is in case the process did not finish clearing the remote database..
+    try:
+        if remote_col.count_documents({}) != 0:
+            os.system(command)
+            print('Successfully performed the mongodump command.')
+            remote_col.drop()
+            print('Successfully dropped the remote database.')
+        else:
+            print(f'Your collection, {remote_col}, was empty at first check.')
+    except:
+        print('''There was an exception of some sort in after_party.py.
+              Trying to go about it with copy_docs().''')
+        db_ops.copy_docs(
+            remote_col,
+            config.client,
+            config.database,
+            config.weathers_collection,
+            filters={},
+            delete=True
+        )
