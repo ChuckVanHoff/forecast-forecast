342a9c3ddd5761581ba6d4d5ea83adeb853ec286
Flying Dutchman <brian.vanhoff@gmail.com>
2020-12-09 23:46:37 -0500

    solved pipenv problem and added collection drop feature

--- ETL/owm_direct/main.py
+++ ETL/owm_direct/main.py
@@ -9,8 +9,8 @@ import db_ops
 
 hash_list = geo_hash.make()
 locations = geo_hash.decode(hash_list)
-# lim = len(locations)
-lim = 165
+lim = len(locations)
+# lim = 165
 count = 0
 timeplaces = []
 
@@ -44,48 +44,50 @@ if os.path.exists(path):
 # Check the remote database. If there are documents in there, dump them and
 # drop the collection
 coll = config.remote_client[config.database][config.weathers_collection]
-command = f'mongodump --uri={config.uri}/{config.database}'
+command = f'mongodump --uri={config.uri}'
 #mongodb+srv://chuckvanhoff:Fe7ePrX%215L5Wh6W@cluster0-anhr9.mongodb.net/test'
-if db_ops.check_db_access(config.remote_client):
-    if coll.count_documents({}) > 0:
-        try:
-            print('just about to try to give the mongodump command from main.py')
-            result = os.system(command)
-            if result:
-                print('dumpped the collection.')
-                print('not dropping the collection ust yet.')
-            else:
-                print(f'''it did not return anything from the command. I was 
-                        expecting a true/false return.
-                        result = {result}''')
-        except:
-            print('just got an error in main.py around line 45.')
-    #     if coll.drop():
-    #         print('dropped the collection')
-else:
-    print("you have no db access!")
 
 
 if __name__ == '__main__':
     # Create an empty log file:
     with open(path, 'w') as pl:
         print('Started new progress log.')
+    
     # Start a loop that will continue until all the locations have been requested
     # for, making sure that when an error occurs that is not handled otherwise
     # the pinky party restarts where is last had a success.
     while count < lim:
+        if db_ops.check_db_access(config.remote_client):
+            try:
+                pinky.party(locations[count:lim], client=config.remote_client)
+                break
+            except KeyboardInterrupt:
+                break
+            except pymongo.errors.OperationFailure:
+                db = config.remote_client[config.database][config.weathers_collection]
+            except:
+                print('Got error. Restarting pinky.    from the end of the log file.')
+                pinky.party(locations[count:lim], client=config.remote_client)
+                # Incriment the counter before continuing the loop at the last element.
+                count = 0
+                for line in open(path, 'r'):
+                    count += 1
+        else:
+            print("you have no db access!")
+    # Check the database to be sure the data was loaded, then try to dump it and
+    # if the dump was a success, then try to drop it.
+    if coll.count_documents({}) > 0:
         try:
-            pinky.party(locations[count:lim], client=config.remote_client)
-            break
-        except KeyboardInterrupt:
-            break
-        except pymongo.errors.OperationFailure:
-            db = config.remote_client[config.database][config.weathers_collection]
+            print('just about to try to give the mongodump command from main.py')
+            result = os.system(command)
+            if result == 0:
+                print('dumped the collection.')
+                coll.drop()
+                print('dropped the collection')
+            else:
+                print(f'''it did not return anything from the command. I was 
+                        expecting a true/false return.
+                        result = {result}''')
         except:
-            print('Got error. Restarting pinky.    from the end of the log file.')
-            pinky.party(locations[count:lim], client=config.remote_client)
-            # Incriment the counter before continuing the loop at the last element.
-            count = 0
-            for line in open(path, 'r'):
-                count += 1
-    os.remove(path)
+            print('just got an error in main.py while trying to dump and drop')
+    os.remove(path)  # Drop the progress log file since the process completed.
