2d7e60fd999fd7b9ef2fadd8f1acaabb029e6831
Brian VanHoff <brian.vanhoff@gmail.com>
2024-11-05 13:30:13 -0500

    testing for push-success

---
+++ gittest.py
@@ -0,0 +1,10 @@
+# This is a test file.
+ 
+# The intent is discovery: to find out if git will allow me to push from
+# this local machine.
+
+# I will create a file, add, commit, and push it to origin/master.
+
+# If there is no exception raised, then I will have confirmation that whatever
+# the issue I'm dealing with in forecast-forecast related to pushing from the
+# local to the remote repository.
